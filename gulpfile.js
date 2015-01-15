45d07983da622d50d75dc79d210ee1f3314a3c60
Anatoly Plutenko <plutenko@redmarker.ru>
2015-01-15 14:42:57 +0300

    add structure for frontend

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    cssmin = require('gulp-cssmin'),
+    compass = require('gulp-compass'),
+    jade = require('jade'),
+    gulpJade = require('gulp-jade'),
+    imageop = require('gulp-image-optimization'),
+    connect = require('gulp-connect'),
+    opn = require('opn'),
+    livereload = require('gulp-livereload');
+
+gulp.task('connect', function() {
+    connect.server({
+        root: '',
+        livereload: true
+    });
+    opn('http://start-project/');
+});
+
+gulp.task('sass', function () {
+    gulp.src('src/sass/style.scss')
+        .pipe(compass({
+            config_file: 'config.rb',
+            css: 'src/css',
+            sass: 'src/sass'
+        }))
+        .pipe(sass())
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(rename('style.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('scripts', function () {
+    gulp.src([
+            'src/js/jquery-1.11.1.min.js'
+        ])
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(uglify())
+        .pipe(rename('scripts.min.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(connect.reload());
+});
+
+gulp.task('jade', function () {
+    return gulp.src('src/jade/**/*.jade')
+        .pipe(gulpJade({
+            jade: jade,
+            pretty: true
+        }))
+        .pipe(gulp.dest(''))
+        .pipe(connect.reload());
+});
+
+gulp.task('images', function(cb) {
+    gulp.src(['src/img/**/*.png','src/img/**/*.jpg','src/img/**/*.gif','src/img/**/*.jpeg']).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('dist/img')).on('end', cb).on('error', cb);
+});
+
+// Действия по умолчанию
+gulp.task('default', function(){
+    gulp.run('sass', 'scripts', 'jade', 'connect');
+    gulp.watch(["src/sass/**/*.scss"], function(event){
+        gulp.run('sass');
+    });
+    gulp.watch(["src/js/*.js"], function(event){
+        gulp.run('scripts');
+    });
+    gulp.watch(["src/jade/**/*.jade"], function(event){
+        gulp.run('jade');
+    });
+});
