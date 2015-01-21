40121f65e9025ef5ec9a6e3e5d10e5a124ba2fb2
Yury Molchanov <0x4d59@gmail.com>
2015-01-21 17:55:24 +0300

    counter buttons js

---
+++ src/js/counterButtons.js
@@ -0,0 +1,27 @@
+var COUNTERBTN = (function () {
+  var 
+      // get button, checkout what direction it is 
+      // and change appropriate window
+      changeCoordValue = function (coordButton) {
+        var
+            direction = coordButton.hasClass('crd-arrow-list__item--up') ? 1 : -1,
+            coordWindow = coordButton.closest('.generator-position-coordinates').find('.crd-window__num');
+
+        coordWindow.text(parseInt(coordWindow.text(), 10) + direction);
+      };
+
+  return {
+
+    init: function () {
+      // $('.crd-arrow-list__item').on('click', function () {
+      //   changeCoordValue($(this));
+      // });
+      $('.crd-arrow-list__item').on('mousedown mouseup', function (event) {
+        changeCoordValue($(this));
+      });
+    }
+
+  }
+})()
+// create DOM ready!
+COUNTERBTN.init();
