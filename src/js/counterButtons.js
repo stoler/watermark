70de1e0bb089b740ff32e8206a2837ead7ec3402
Yury Molchanov <0x4d59@gmail.com>
2015-01-27 14:36:08 +0300

    watermark react on keybord input

--- src/js/counterButtons.js
+++ src/js/counterButtons.js
@@ -2,6 +2,11 @@
 
 var COUNTERBTN = (function () {
   var 
+      initInputValue = function () {
+        $('.crd-window__num').each(function () {
+          $(this).val(0);
+        });
+      },
       // get button, checkout what direction it is 
       // and change appropriate window
       changeCoordValue = function (coordButton) {
@@ -9,12 +14,18 @@ var COUNTERBTN = (function () {
             direction = coordButton.hasClass('crd-arrow-list__item--up') ? 10 : -10,
             coordWindow = coordButton.closest('.generator-position-coordinates').find('.crd-window__num');
 
-        coordWindow.attr('value', parseInt(coordWindow.attr('value')) + direction);
+        // coordWindow.attr('value', parseInt(coordWindow.attr('value')) + direction);
+        coordWindow.val(parseInt(coordWindow.val()) + direction);
+      },
+      changeInputValue = function (inputField) {
+        console.log(inputField);
+        console.log(inputField.val());
       };
 
   return {
 
     init: function () {
+      initInputValue();
       $('.crd-arrow-list__item').on('click', function () {
         changeCoordValue($(this));
       });
