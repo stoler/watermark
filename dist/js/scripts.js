5f54e3b9ebfe5eaa85095fe73522d275e4e0903f
oksanich <oksanich_90@mail.ru>
2015-01-31 21:27:38 +0200

    No changes.

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -10,13 +10,8 @@ var model = (function () {
             'y': 0
         },
         'files': {
-<<<<<<< HEAD
-            image: '',
-            watermark: ''
-=======
             'image': '',
             'watermark': ''
->>>>>>> 82e6f15c8f7a37bf3df4a4d8b1c106fe1e9437db
         },
         'gridType': 'mono',
         'alpha': 1,
@@ -3270,50 +3265,34 @@ var COUNTERBTN = (function () {
 })();
 var PLACEGRID = (function () {
     var
-        // состоянии, которое не позволяет делать дом манипуляции если нажать на свитч, который
-        // и так выбран
+    // состоянии, которое не позволяет делать дом манипуляции если нажать на свитч, который
+    // и так выбран
         state = '',
 
-        // линия на гриде
+    // линия на гриде
         gridLines = $('.multi-line'),
 
-        // квадрат на гриде
+    // квадрат на гриде
         squareOfGrid = $('.square-td'),
 
-        // стрелки или буквы возле полей инпута
+    // стрелки или буквы возле полей инпута
         inputLabels = $('.crd-axis'),
 
-<<<<<<< HEAD
-        // изменяет ширину линий грида
+    // изменяет ширину линий грида
         changeLineWidth = function () {
             var
                 gridSide = 101,
-                lineWidthX = model.margins.x,
-                lineWidthY = model.margins.y,
-                // вычисляем центр
-                leftPosition = (gridSide - lineWidthX) / 2,
-                topPosition = (gridSide - lineWidthY) / 2;
-
-            $('.multi-line.position-vertical').css({width: lineWidthX, left: leftPosition});
-            $('.multi-line.position-horizontal').css({height: lineWidthY, top: topPosition});
-        },
-=======
-      // изменяет ширину линий грида
-      changeLineWidth = function () {
-        var
-            gridSide = 101,
-            lineWidthVertical = model.margins.y,
-            lineWidthHorizontal = model.margins.x,
+                lineWidthVertical = model.margins.y,
+                lineWidthHorizontal = model.margins.x,
             // вычисляем центр
-            leftPosition = (gridSide - lineWidthVertical) / 2,
-            topPosition = (gridSide - lineWidthHorizontal) / 2;
+                leftPosition = (gridSide - lineWidthVertical) / 2,
+                topPosition = (gridSide - lineWidthHorizontal) / 2;
 
-        $('.multi-line.position-vertical').css({width: lineWidthVertical, left: leftPosition});
-        $('.multi-line.position-horizontal').css({height: lineWidthHorizontal, top: topPosition});
-      },
->>>>>>> grid
+            $('.multi-line.position-vertical').css({width: lineWidthVertical, left: leftPosition});
+            $('.multi-line.position-horizontal').css({height: lineWidthHorizontal, top: topPosition});
+        },
 
-        // меняет квадраты на линии
+    // меняет квадраты на линии
         squareToLine = function () {
             // показываем линии
             gridLines.each(function () {
@@ -3330,7 +3309,7 @@ var PLACEGRID = (function () {
             squareOfGrid.off('click');
         },
 
-        // меняет линии на квадраты
+    // меняет линии на квадраты
         lineToSquare = function () {
             // подключаем ховер по квадратам
             squareOfGrid.each(function () {
@@ -3348,9 +3327,11 @@ var PLACEGRID = (function () {
                 $('.square-td--active').removeClass('square-td--active');
                 $(this).addClass('square-td--active');
             })
+
+
         },
 
-        // меняет названия полей ввода с букв на стрелки
+    // меняет названия полей ввода с букв на стрелки
         changeLabels = function (mode) {
             if (mode === 'mono') {
                 inputLabels.each(function () {
@@ -3393,70 +3374,39 @@ var PLACEGRID = (function () {
                 // обновлять стиль только в районе толщины линий
                 changeLineWidth();
             }
+        },
+        updateModel: function (square) {
+            var
+            // Размеры элементов
+                watermark = $('.generator-picture__watermark'),
+                images = $('.generator-picture__image'),
+                imagesWidth = images.width(),
+                imageHalfWidth = imagesWidth / 2,
+                watermarkWidth = watermark.width(),
+                watermarkHalfWidth = watermarkWidth/ 2,
+                imageHeight = images.height(),
+                imageHalfHeight = imageHeight / 2,
+                watermarkHeight = watermark.height(),
+                watermarkHalfHeight = watermarkHeight/ 2,
+                centerX = imageHalfWidth-watermarkHalfWidth,
+                centerY = imageHalfHeight-watermarkHalfHeight,
+                gridPosArr = [
+                    [0, 0],
+                    [0, centerX],
+                    [0, imagesWidth-watermarkWidth],
+                    [centerY, 0],
+                    [centerY, centerX],
+                    [centerY, imagesWidth-watermarkWidth],
+                    [imageHeight-watermarkHeight, 0],
+                    [imageHeight-watermarkHeight, centerX],
+                    [imageHeight-watermarkHeight, imagesWidth-watermarkWidth]
+                ],
+
+                index = $('.square-td').index(square);
+            // toFixed чтобы не было значения в полпикселя
+            model.coord.x = parseInt(gridPosArr[index][1].toFixed(0));
+            model.coord.y = parseInt(gridPosArr[index][0].toFixed(0));
         }
-      },
-
-      changeGrid = function (mode) {
-        if (mode === 'mono') {
-          // запускает функцию из линии в квадрат
-          lineToSquare();
-        } else {
-          // запускает функцию из квадрата в линии
-          squareToLine();
-        }
-      };
-
-  return {
-    init: function () {
-      state = 'mono';
-      lineToSquare();
-    },
-    setStyle: function () {
-      var 
-          mode = model.gridType === 'mono' ? 'mono' : 'multi';
-      // не запускать, если нажали по активной кнопке свитча
-      if (mode !== state) {
-        state = mode;
-        changeLabels(mode);
-        changeGrid(mode);
-      }
-      if (state === 'multi') {
-        // обновлять стиль только в районе толщины линий
-        changeLineWidth();
-      }
-    },
-    updateModel: function (square) {
-      var
-          // Размеры элементов
-          watermark = $('.generator-picture__watermark'),
-          images = $('.generator-picture__image'),
-          imagesWidth = images.width(),
-          imageHalfWidth = imagesWidth / 2,
-          watermarkWidth = watermark.width(),
-          watermarkHalfWidth = watermarkWidth/ 2,
-          imageHeight = images.height(),
-          imageHalfHeight = imageHeight / 2,
-          watermarkHeight = watermark.height(),
-          watermarkHalfHeight = watermarkHeight/ 2,
-          centerX = imageHalfWidth-watermarkHalfWidth,
-          centerY = imageHalfHeight-watermarkHalfHeight,
-          gridPosArr = [
-              [0, 0],
-              [0, centerX],
-              [0, imagesWidth-watermarkWidth],
-              [centerY, 0],
-              [centerY, centerX],
-              [centerY, imagesWidth-watermarkWidth],
-              [imageHeight-watermarkHeight, 0],
-              [imageHeight-watermarkHeight, centerX],
-              [imageHeight-watermarkHeight, imagesWidth-watermarkWidth]
-          ],
-
-          index = $('.square-td').index(square);
-      // toFixed чтобы не было значения в полпикселя
-      model.coord.x = parseInt(gridPosArr[index][1].toFixed(0));
-      model.coord.y = parseInt(gridPosArr[index][0].toFixed(0));
->>>>>>> grid
     }
 })();
 var INPUTFIELD = (function () {
